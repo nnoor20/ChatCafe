b14c6091ac622f52765b44b6f4cdd3214ecb29ae
bednie <88800117+bednie@users.noreply.github.com>
2023-04-29 21:32:12 +0000

    CF-43 unit test stub

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,51 +1,57 @@
 import unittest
 from flask import json
-from app import app, menu, messages
+from app import app, menu, rooms, socketio
+from flask_socketio import SocketIOTestClient
+import time
 
 class TestFlaskApp(unittest.TestCase):
 
     def setUp(self):
         self.app = app.test_client()
+        self.socketio_test_client = SocketIOTestClient(app, socketio)
 
     def test_welcome(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Welcome to the Cafe', response.data)
+        self.assertIn(b'MSU Denver Chat Cafe', response.data)
 
     def test_order_get(self):
         response = self.app.get('/order')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Place your order', response.data)
+        self.assertIn(b'Order Confirmation', response.data)
 
     def test_order_post(self):
         response = self.app.post('/order', data={
             "name": "John Doe",
             "student_id": "12345",
-            "items": ["coffee", "croissant"]
+            "total_price1": "5.48",
         })
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Thank you for you order, John Doe (12345)! Your order has been placed. Your total is $5.48.', response.data)
+        self.assertIn(b'Thank you for you order, John Doe, (12345)! Your order has been placed. Your total is $5.48.', response.data)
 
-    def test_chat_get(self):
-        response = self.app.get('/chat')
+    def test_index_get(self):
+        response = self.app.get('/index')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Enter your username', response.data)
 
-    def test_chat_post(self):
-        response = self.app.post('/chat', data={
-            "username": "JohnDoe"
-        })
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Welcome, JohnDoe!', response.data)
+    def test_connect_and_send_message(self):
+        self.socketio_test_client.connect(namespace='/chat')
+        time.sleep(0.5)
+        print("Connected:", self.socketio_test_client.is_connected(namespace='/chat'))  # Add this line
+        response = self.socketio_test_client.emit('message', {'data': 'Hello, world!'}, namespace='/chat')
+        received = self.socketio_test_client.get_received()
 
-    def test_send_message(self):
-        response = self.app.post('/send', data={
-            "username": "JohnDoe",
-            "message": "Hello, world!"
-        })
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Hello, world!', response.data)
-        self.assertIn(("JohnDoe", "Hello, world!"), messages)
+        self.assertEqual(len(received), 1)
+        self.assertEqual(received[0]['name'], 'message')
+        self.assertIn('Hello, world!', received[0]['args'][0]['message'])
+        self.socketio_test_client.disconnect(namespace='/chat')
+
+    def test_disconnect(self):
+        self.socketio_test_client.connect()
+        self.socketio_test_client.disconnect()
+
+        response = self.socketio_test_client.is_connected()
+        self.assertFalse(response)
 
 if __name__ == '__main__':
     unittest.main()
